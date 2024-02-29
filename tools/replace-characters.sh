17c0d3f0f535fece390bd2b26b14f939e33b1a03
Zhou Shenmin <shenminzhou@gmail.com>
2024-02-29 21:35:21 +0900

    230

--- tools/replace-characters.sh
+++ tools/replace-characters.sh
@@ -27,6 +27,8 @@ git diff --name-only --cached | xargs -i \
   s|被遺|被遣|g
   s|^大明|天明|g
   s|差和|差扣|g
+  s|^又化|文化|g
+  s|掛(=|〓)$|掛リ|g
   /^(曰|日|目|戸|E|ヨ|〓)$/d
   ' {}
-# fd -e txt -e yml -x sed -i -e "s|差和|差扣|g" 
+# fd -e txt -e yml -x sed -i -e "s|^又化|文化|g" 
