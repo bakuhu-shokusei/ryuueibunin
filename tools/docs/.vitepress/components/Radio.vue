6b877c470e21e86c602ec7e1f371c285918cbe52
Zhou Shenmin <shenminzhou@gmail.com>
2024-04-24 20:13:54 +0900

    fix svg color

--- tools/docs/.vitepress/components/Radio.vue
+++ tools/docs/.vitepress/components/Radio.vue
@@ -4,25 +4,18 @@
       <div class="radio-input">
         <i class="v-icon" aria-hidden="true">
           <svg
-            v-if="checked"
-            class="v-icon__svg"
             xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
             viewBox="0 0 24 24"
             role="img"
             aria-hidden="true"
           >
             <path
+              v-if="checked"
               d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17A5,5 0 0,0 17,12A5,5 0 0,0 12,7Z"
             ></path>
-          </svg>
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            role="img"
-            aria-hidden="true"
-          >
             <path
+              v-else
               d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
             ></path>
           </svg>
